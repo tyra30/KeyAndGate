b2d6d863dd80a467b1c362dcbd70339eaf97358d
TYRA EKSTRAND <ekstrandtyra@gmail.com>
2021-10-13 14:27:39 +0200

    AvatarClass

--- KeyAndGate/Avatar.cs
+++ KeyAndGate/Avatar.cs
@@ -4,6 +4,37 @@ namespace KeyAndGate
 {
     public class Avatar
     {
+
+    private Rectangle rect = new Rectangle();
+    private Key CarriedKey = { get; set; }
+    private float speed = 6f();
+    public Avatar()
+    {
         
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Draw()
+    {
+
+    }
+
+    public void CheckKeyCollision()
+    {
+
+    }
+
+    public void CheckDoorCollision()
+    {
+
+    }
+
+
+
+
     }
 }
